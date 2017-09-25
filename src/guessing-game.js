c8d16afa09b8e2a957ee1bca774211bbd57ec5e4
Yaroslav Anikovich <yasyamail.ru@mail.ru>
2017-09-25 23:02:23 +0300

    Guessing-game completed

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
-
+    constructor() {
+     this.minimal = 0;
+     this.mxml = 0;
+     this.current = Math.round(((this.mxml + this.minimal)/2));
     }
 
-    guess() {
+    setRange(min, max) {
+	this.minimal = min;
+	this.mxml = max;
+	this.current =  Math.round(((this.mxml + this.minimal)/2));
+    }
 
+    guess() { 
+      return this.current;
     }
 
     lower() {
-
+     this.mxml= this.current;
+     this.current = Math.round(((this.mxml + this.minimal)/2));
     }
 
     greater() {
-
+     this.minimal = this.current;
+     this.current = Math.round(((this.mxml + this.minimal)/2));
     }
 }
 
